ea3e57764a6c76deb91def11f7738cd837978f36
guifaxina <99680960+guifaxina@users.noreply.github.com>
2022-08-09 21:21:44 -0300

    added: database structure
    now users can be stored at database

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/api-w-type')
 
 app.use(express.json());
 app.get("/", (req, res) => {
@@ -9,4 +12,4 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
   console.log("Server running on port 3000 ❤️");
-});
+});''
